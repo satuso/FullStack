f97b821f54d6ed9d717f6010b3005898a92764b8
satuso <73895314+satuso@users.noreply.github.com>
2021-08-29 10:57:13 +0300

    1.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,41 +1,43 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 const Part = (props) => {
   return (
     <>
-    <p>{props.name1} {props.exercises1}</p>
-    <p>{props.name2} {props.exercises2}</p>
-    <p>{props.name3} {props.exercises3}</p>
+      <p>{props.name1} {props.exercises1}</p>
+      <p>{props.name2} {props.exercises2}</p>
+      <p>{props.name3} {props.exercises3}</p>
     </>
   )
 }
@@ -43,9 +45,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-    <Part name1={props.parts[0].name} exercises1={props.parts[0].exercises}/>
-    <Part name2={props.parts[1].name} exercises2={props.parts[1].exercises}/>
-    <Part name3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
+      <Part name1={props.parts[0].name} exercises1={props.parts[0].exercises}/>
+      <Part name2={props.parts[1].name} exercises2={props.parts[1].exercises}/>
+      <Part name3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
   </>
   )
 }
