c03387e4f017ba8d058a038986d03a94729c9d2a
satuso <73895314+satuso@users.noreply.github.com>
2021-08-31 12:31:22 +0300

    1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -8,9 +8,10 @@ const Button = (props) => {
 
 const StatisticLine = (props) => {
   return (
-  <>
-  <p>{props.text} {props.value}</p>
-  </>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
   )
 }
 
@@ -21,21 +22,24 @@ const Statistics = (props) => {
   if (props.good > 0 || props.neutral > 0 || props.bad > 0){
     return (
       <>
-        <h2>statistics</h2>
-        <StatisticLine text="good" value={props.good} />
-        <StatisticLine text="neutral" value={props.neutral} />
-        <StatisticLine text="bad" value={props.bad} />
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
-      </>
+      <h2>statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
+    </>
     )
   } else {
     return (
       <>
         <h2>statistics</h2>
         <p>no feedback given</p>
-        
       </>
     )
   }
