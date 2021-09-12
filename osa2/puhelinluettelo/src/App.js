81dc3eba465f14032b782cb5ef4f91013a369876
satuso <73895314+satuso@users.noreply.github.com>
2021-09-13 00:19:40 +0300

    2.19

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
 import Filter from "./components/Filter"
+import Notification from "./components/Notification"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import personService from "./services/persons"
 
 const App = () => {
-
   useEffect(() => {
     personService
       .getAll()
@@ -18,11 +18,12 @@ const App = () => {
   const [ newName, setNewName ] = useState("")
   const [ newNumber, setNewNumber ] = useState("")
   const [ filter, setFilter ] = useState("")
+  const [ msg, setMsg ] = useState(null)
 
-  let filteredPersons = (filter === "" ? persons : persons.filter(function(person){
+  const filteredPersons = (filter === "" ? persons : persons.filter(function(person){
     return person.name.toLowerCase().includes(filter.toLowerCase())
   }))
-
+ 
   const handleSubmit = (event) => {
     event.preventDefault()
     const person = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
@@ -38,6 +39,10 @@ const App = () => {
       .update(person.id, updatedPerson)
       .then(returnedPerson => {
         setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+        setMsg(`${newName}'s number was updated`)
+        setTimeout(() => {
+          setMsg(null)
+        }, 5000)
       }) : setPersons(filteredPersons)
     } else {
       personService
@@ -46,35 +51,41 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewName(newName)
         setNewNumber(newNumber)
+        setMsg(`Added ${newName}`)
+        setTimeout(() => {
+          setMsg(null)
+        }, 5000)
       })
     }
   }
-
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
-  
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-
   const handleDelete = (id, name) => {
     window.confirm(`Delete ${name}?`) ?
     personService
       .deletePerson(id)
       .then(() => {
-      const newPersons = persons.filter((item) => item.id !== id);
+      const newPersons = persons.filter((item) => item.id !== id)
       setPersons(newPersons)
-    }) : setPersons(filteredPersons)
+      setMsg(`Deleted ${newName}`)
+      setTimeout(() => {
+        setMsg(null)
+      }, 5000)
+      })
+      : setPersons(filteredPersons)
   }
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={msg}/>
       <Filter handleFilterChange={handleFilterChange}/>
       <PersonForm handleSubmit={handleSubmit} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
