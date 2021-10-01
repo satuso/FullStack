26f2ce40a39dfe706cc3794f0a646059cf0888cf
satuso <73895314+satuso@users.noreply.github.com>
2021-10-01 15:26:37 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   if (blogs) return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0){
+    return 0
+  } else {
+    const reducer = (sum, item) => {
+      console.log(sum, item.likes)
+      return sum + item.likes
+    }
+    return blogs.reduce(reducer, 0)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
