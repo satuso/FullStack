ee003f4688c5d0aea043ad70392484db7bb1f92f
satuso <73895314+satuso@users.noreply.github.com>
2021-10-09 12:32:27 +0300

    4.13

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -6,6 +6,13 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const id = Number(request.params.id)
+  const blogs = await blogs.find(blog => {
+    blog.id === id})
+  response.json(blogs)
+})
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -18,6 +25,15 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -78,6 +78,25 @@ test('a blog without title and url is not added', async () => {
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
+  )
+
+  const title = blogsAtEnd.map(r => r.title)
+
+  expect(title).not.toContain(blogToDelete.title)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
