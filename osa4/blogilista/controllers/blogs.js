b3cd1ecda0a7563e3c4cca7fd0cd29c143a87302
satuso <73895314+satuso@users.noreply.github.com>
2021-10-10 15:13:43 +0300

    4.20

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,14 +2,7 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -24,11 +17,11 @@ blogsRouter.get('/:id', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.tokenExtractor, async (request, response) => {
   const body = request.body
-
-  const token = getTokenFrom(request)
+  const token = request.token
   const decodedToken = jwt.verify(token, process.env.SECRET)
+
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
