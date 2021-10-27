999d0b0b75633cdc93532764d57e16b59cc3c1d9
satuso <73895314+satuso@users.noreply.github.com>
2021-10-27 16:06:32 +0300

    5.14

--- osa5/bloglist-frontend/src/components/blog.test.js
+++ osa5/bloglist-frontend/src/components/blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('test blog component', () => {
@@ -17,16 +17,30 @@ describe('test blog component', () => {
   const user = blog.user.name
 
   test('renders title and author', () => {
-
     const component = render(
-      <Blog blog={blog} user={user}/>
+      <Blog
+        blog={blog}
+        user={user}
+      />
     )
+    expect(component.container).toHaveTextContent('title')
+    expect(component.container).toHaveTextContent('author')
 
-    expect(component.container).toHaveTextContent(
-      'title'
-    )
     expect(component.container.user).toBeUndefined()
     expect(component.container.likes).toBeUndefined()
   })
 
+  test('renders url and likes when view button is pressed', () => {
+    const component = render(
+      <Blog
+        blog={blog}
+        user={user}
+      />
+    )
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent(blog.likes)
+    expect(component.container).toHaveTextContent(blog.url)
+  })
 })
