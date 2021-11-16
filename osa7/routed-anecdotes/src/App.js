630f7eafc78b3d389caa5763638d18e4eaa8b879
satuso <73895314+satuso@users.noreply.github.com>
2021-11-16 16:37:24 +0200

    7.4

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -5,6 +5,7 @@ import {
   BrowserRouter as Router,
   Switch, Route, Link
 } from "react-router-dom"
+import  { useField } from './hooks'
 
 const Menu = ({addNew, anecdotes, notification}) => {
   const padding = {
@@ -83,17 +84,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
   const history = useHistory()
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push("/")
@@ -105,15 +107,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input  {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input  {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input  {...info} />
         </div>
         <button>create</button>
       </form>
@@ -148,7 +150,7 @@ const App = () => {
     setNotification(`a new anecdote ${anecdote.content} created!`)
     setTimeout(() => {
       setNotification(null)
-    }, 5000)
+    }, 10000)
   }
 
   const anecdoteById = (id) =>
