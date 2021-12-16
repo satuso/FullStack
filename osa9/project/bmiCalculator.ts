822ff15a7ca3b8d42ab0c50ed94cf7adc0260779
satuso <73895314+satuso@users.noreply.github.com>
2021-12-16 16:50:46 +0200

    9.3

--- osa9/project/bmiCalculator.ts
+++ osa9/project/bmiCalculator.ts
@@ -1,18 +1,46 @@
+interface Bmi {
+  value1: number;
+  value2: number;
+}
+
 const calculateBmi = (a: number, b: number) => {
   const c = a / 100;
   const bmi = b / (c * c);
   if (bmi < 18.4) {
-    return `${bmi.toFixed(2)} Underweight (unhealthy)`
-  };
+    return `${bmi.toFixed(2)} Underweight (unhealthy)`;
+  }
   if (bmi >= 18.5 && bmi <= 24.9){
-    return `${bmi.toFixed(2)} Normal (healthy weight)`
-  };
+    return `${bmi.toFixed(2)} Normal (healthy weight)`;
+  }
   if (bmi >= 25 && bmi <= 29.9){
-    return `${bmi.toFixed(2)} Overweight (at risk)`
-  };
+    return `${bmi.toFixed(2)} Overweight (at risk)`;
+  }
   if (bmi >= 30){
-    return `${bmi.toFixed(2)} Obese (unhealthy)`
-  };
+    return `${bmi.toFixed(2)} Obese (unhealthy)`;
+  }
 };
 
-console.log(calculateBmi(180, 74));
+const parseArgsBmi = (args: Array<string>): Bmi => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+try {
+  const { value1, value2 } = parseArgsBmi(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- osa9/project/exerciseCalculator.ts
+++ osa9/project/exerciseCalculator.ts
@@ -6,32 +6,31 @@ interface Result {
   ratingDescription: string,
   target: number,
   average: number
-};
+}
 
 const calculateExercises = (array: Array<number>, target: number) : Result => {
-    const average = array.reduce(function (avg, value, _, { length }) {
-      return avg + value / length;
-    }, 0);
-    const periodLength = array.length
-    const trainingDays = array.filter(value => value !== 0).length
-    let rating = 0;
-    let success = false;
-    let ratingDescription = ''
+  const average = array.reduce(function (avg, value, _, { length }) {
+    return avg + value / length;
+  }, 0);
+  const periodLength = array.length;
+  const trainingDays = array.filter(value => value !== 0).length;
+  let rating = 0;
+  let success = false;
+  let ratingDescription = '';
   
-    if (average >= target){
-      success = true
-      rating = 3
-      ratingDescription = 'excellent, you have reached your target!'
-    } else if (average > target * 0.5){
-      success = false
-      rating = 2
-      ratingDescription = 'not too bad but could be better'
-    } else {
-      success = false
-      rating = 1
-      ratingDescription = 'not enough exercise'
-    };
-
+  if (average >= target){
+    success = true;
+    rating = 3;
+    ratingDescription = 'excellent, you have reached your target!';
+  } else if (average > target * 0.5){
+    success = false;
+    rating = 2;
+    ratingDescription = 'not too bad but could be better';
+  } else {
+    success = false;
+    rating = 1;
+    ratingDescription = 'not enough exercise';
+  }
   return {
     periodLength,
     trainingDays,
@@ -39,8 +38,34 @@ const calculateExercises = (array: Array<number>, target: number) : Result => {
     rating,
     ratingDescription,
     target,
-    average,
+    average
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+interface Exercise {
+  value1: number,
+  value2: number []
+}
+
+const parseArgsExercises = (args: Array<string>): Exercise => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+    return {
+      value1: Number(args[2]),
+      value2: args.map(arg => Number(arg)).slice(3),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+try {
+  const { value1, value2 } = parseArgsExercises(process.argv);
+  console.log(calculateExercises(value2, value1));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
