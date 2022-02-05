b9f2fe9348e08f51ed6d3063ff7cae1edc98cb0b
satuso <73895314+satuso@users.noreply.github.com>
2022-02-05 19:07:55 +0200

    9.17

---
+++ osa9/patientor-frontend/src/PatientPage/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { apiBaseUrl } from "../constants";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+import { Icon } from "semantic-ui-react";
+
+const PatientPage = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+  const patient = patients[id];
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "GET_PATIENT", payload: patientFromApi });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    void fetchPatient();
+  }, [dispatch]);
+
+  if (!patient) return null;
+
+  return (
+    <div>
+      <h2>{patient.name} {patient.gender === "male" && <Icon name="mars"></Icon> || patient.gender === "female" && <Icon name="venus"></Icon> || patient.gender === "other" && <Icon name="transgender"></Icon>}</h2>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientPage;
